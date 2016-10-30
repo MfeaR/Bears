130e886ffcabe33e8acd3ac7f87491cf66ae0ad0
MfeaR <mfear13@gmail.com>
2016-10-31 00:19:10 +0300

    change count

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var songLog = {
+    count:99, //Cчетчик
+
     definitionOfTheEnd: function (data) { //Функция определния окончания
         var remainderOfTheDivision = data % 10; //Определяем остаток от деления
         if (remainderOfTheDivision == 1) {
@@ -10,19 +12,18 @@ var songLog = {
         }
     },
     generateSongText: function () { //Функция генерации текста
-        var count = 99; //Cчетчик
         var firstEnd, secondEnd; // Окончания предложений
-        while (count > 0) {
+        while (this.count > 0) {
 
-            firstEnd = this.definitionOfTheEnd(count);// Определяем первое окончание
+            firstEnd = this.definitionOfTheEnd(this.count);// Определяем первое окончание
             console.log("");
-            console.log(count + " бутыл" + firstEnd + " пива на стене.");
-            console.log(count + " бутыл" + firstEnd + " пива!");
+            console.log(this.count + " бутыл" + firstEnd + " пива на стене.");
+            console.log(this.count + " бутыл" + firstEnd + " пива!");
             console.log("Возьми одну, пусти по кругу.");
-            count--; //Уменьшаем счетчик
-            secondEnd = this.definitionOfTheEnd(count); // Определяем второе окончание
-            if (count != 0) {
-                console.log(count + " бутыл" + secondEnd + " пива на стене!");
+            this.count--; //Уменьшаем счетчик
+            secondEnd = this.definitionOfTheEnd(this.count); // Определяем второе окончание
+            if (this.count != 0) {
+                console.log(this.count + " бутыл" + secondEnd + " пива на стене!");
             } else {
                 console.log("Не осталось бутылок пива на стене.");
             }
